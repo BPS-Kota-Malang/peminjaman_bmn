cc0adc6d03c83a38619cc1fbeb920611ac198e02
wahyufajar879 <goesfa3110@gmail.com>
2022-09-08 20:55:17 +0700

    page

---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.dashboard')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Role Management</h2>
+        </div>
+        <div class="pull-right">
+        @can('role-create')
+            <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
+            @endcan
+        </div>
+    </div>
+</div>
